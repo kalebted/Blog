c0629541d93c6724a721cf73e0267045e0d1c2ab
thilaks <thilak7c@gmail.com>
2022-08-27 23:16:30 +0800

    I have pushed the code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+import BlogIndex from "pages/BlogIndex";
+import ThankYouPage from "ThankYouPage.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <BlogIndex />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
