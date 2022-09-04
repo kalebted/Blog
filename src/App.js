ed4dbbe5976df3641eed64ce5248e8dcff581d3d
thilaks <thilak7c@gmail.com>
2022-09-05 02:45:24 +0800

    I have fixed the error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 import React from "react";
-import { css } from "styled-components/macro"; //eslint-disable-line
+// import { css } from "styled-components/macro"; //eslint-disable-line
 
 import BlogIndex from "pages/BlogIndex";
-import ThankYouPage from "ThankYouPage.js";
+// import Blog from "Blog.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
